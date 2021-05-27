a28c88355187ca7de9f12d0b2eee92643a9d01c6
Trista Cheng <trista8130@gmail.com>
2021-05-27 17:08:05 -0400

    update

--- src/Api/apiList.js
+++ src/Api/apiList.js
@@ -3,7 +3,7 @@ const searchurl = (ARTIST_NAME) =>
   `term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=500`;
 
  const getAllAlbums = (input) =>
-  fetch([baseurl, searchurl(input)].join("")).then((response) =>
-    response.json()
-  );
+   fetch([baseurl, searchurl(input)].join(""), { mode: "no-cors" }).then(
+     (response) => response.json()
+   );
 export default getAllAlbums
